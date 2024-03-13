8cc4b1cd635ea0d7ffe69b933b1e7c9aac613456
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-03-13 23:38:17 +0000

    tidy up

--- MweSkeletons/broken_jump_to_def.lean
+++
@@ -1,5 +0,0 @@
-import MweSkeletons.CommRing -- includes a new definition of CommRing independent of mathlib
-
-#check CommRing -- hover says `import MweSkeletons.CommRing`
--- but "jump to definition" and "jump to declaration" both jump to mathlib
--- despite no mathlib files being imported here.
